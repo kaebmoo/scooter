99336a6028e2e702cb47b964a4003e67eb760521
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2023-04-26 00:27:29 +0700

    add to lastseen table

--- subscribe.py
+++ subscribe.py
@@ -6,6 +6,38 @@ import calendar
 import time
 from datetime import datetime
 import pandas as pd
+import boto3
+from pprint import pprint
+from boto3.dynamodb.conditions import Key
+
+# set up dynamodb and table
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
+# on the table resource are accessed or its load() method is called.
+table_scooter = dynamodb.Table('scooter')
+table_lastseen = dynamodb.Table('scooter_lastseen')
+
+def query_data(beacon):
+    print("searching for ", beacon)
+    response = table_lastseen.query(
+        KeyConditionExpression="lastseen = :beacon",
+        ExpressionAttributeValues={
+        ":beacon": beacon,
+        },
+    )
+    pprint(response["Items"])
+    print(f"ScannedCount: {response['ScannedCount']}")
+    return(response["Items"])
+
+def add_data(items):
+    print(items)
+    response = table_lastseen.put_item(Item = items)
+
 
 # อ่านข้อมูลจาก mqtt broker
 # จำลองการรับ beacon data จาก mqtt broker
@@ -13,15 +45,17 @@ import pandas as pd
 def on_message(client, userdata, message):
     # convert message payload to json object in python
     msg_payload = message.payload.decode("utf-8")
-    print(type(msg_payload)) # debug message
+    # print(type(msg_payload)) # debug message
     msg = json.loads(msg_payload)
-    print(type(msg)) # debug message
+    # print(type(msg)) # debug message
     beacon_msg = json.loads(msg)
     print(type(beacon_msg)) # debug message
+    '''
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
+    '''
     print(beacon_msg["device_id"])
     print(beacon_msg["beacon_id"])
     print(beacon_msg["timestamp"])
@@ -31,6 +65,19 @@ def on_message(client, userdata, message):
         is_present = True
     elif beacon_msg["rssi"] < -70:
         is_present = False 
+    beacon_msg['is_present'] = is_present
+    beacon_msg['lastseen'] = beacon_msg["beacon_id"]
+    # beacon_msg['lastseen'] = str(int(time.time()))
+    # beacon_msg["rssi"] = str(beacon_msg["rssi"])
+    print(json.dumps(beacon_msg, indent=4))
+    is_in_table = query_data(beacon_msg["beacon_id"]) # query beacon_id from lastseen table
+    # if not present in table 
+    if not is_in_table:
+        print("beacon_id not present in lastseen table. adding it")
+        add_data(beacon_msg) 
+    else:
+        print("found the beacon")
+        print(is_in_table)
 
 def on_publish(client,userdata,result):             #create function for callback
     print("data published to thingsboard \n")
@@ -53,13 +100,31 @@ def subscribe_data():
     client1.subscribe("v1/devices/me/telemetry")
     client1.loop_forever() #start the loop
 
-station_db = pd.read_csv("station_db.csv", converters={"device_id":str})
-print(station_db)
-station_db["deviceid_beaconlist"] = '"'+station_db["device_id"]+'":' + station_db["beacon_list"]
-print(station_db.to_json(orient='records'))
+def beacon_list(device_id):
+    station_db = pd.read_csv("station_db.csv", converters={"device_id":str})
+    # print(station_db)
 
-print(station_db["deviceid_beaconlist"])
-beacon_list = station_db["beacon_list"].tolist()
-print(beacon_list[0])
-beacon_json = station_db["beacon_list"].to_json(orient='records')
-print(beacon_json[0])
+    row_index = station_db.index[station_db['device_id'] == device_id].tolist()
+    # print(row_index) return row index for specific device_id
+    json_object = station_db.loc[row_index, "beacon_list"]
+    # print(json_object)
+
+    # beacon list 
+    for device_id in json_object:
+        device = json.loads(device_id)
+        device_list = []
+        for device_element in device:
+            # print(device_element['S'])
+            device_list.append(device_element['S'])
+
+    return(device_list)
+
+
+def main():
+    print()
+    print(beacon_list("5046452"))
+
+    subscribe_data()
+
+if __name__ == "__main__":
+    main()
--- transaction_db.py
+++ transaction_db.py
@@ -1,4 +1,20 @@
 import boto3
+from pprint import pprint
+from boto3.dynamodb.conditions import Key
+
+def query_data(beacon):
+    response = table_lastseen.query(
+        KeyConditionExpression="beacon_list = :beacon",
+        ExpressionAttributeValues={
+        ":beacon": beacon,
+        },
+    )
+    pprint(response["Items"])
+    print(f"ScannedCount: {response['ScannedCount']}")
+    return(response["Items"])
+
+def add_data():
+    pass
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
@@ -8,9 +24,16 @@ dynamodb = boto3.resource('dynamodb')
 # are lazy-loaded: a request is not made nor are the attribute
 # values populated until the attributes
 # on the table resource are accessed or its load() method is called.
-table = dynamodb.Table('scooter')
+table_scooter = dynamodb.Table('scooter')
+table_lastseen = dynamodb.Table('scooter_lastseen')
 
 # Print out some data about the table.
 # This will cause a request to be made to DynamoDB and its attribute
 # values will be set based on the response.
-print(table.creation_date_time)
+print(table_scooter.creation_date_time)
+print(table_lastseen.creation_date_time)
+is_in_table = query_data("E0702891-AE8E-8DC0-B0DE-3C666B326CA9:3138:0055")
+print(is_in_table)
+print(type(is_in_table))
+if not is_in_table:
+    print("not found.")
